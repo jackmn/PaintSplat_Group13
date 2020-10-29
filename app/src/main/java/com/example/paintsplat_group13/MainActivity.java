074511bae0a102469a0fc971cab7bc400835b790
DavidMcMorrow <mcmorrda@tcd.ie>
2020-10-29 14:01:12 +0000

    finished watching the video

--- app/src/main/java/com/example/paintsplat_group13/MainActivity.java
+++ app/src/main/java/com/example/paintsplat_group13/MainActivity.java
@@ -1,14 +1,90 @@
 package com.example.paintsplat_group13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    String playerName = "";
+    String roomName= "";
+    String role = "";
+    String message = "";
+
+    FirebaseDatabase database;
+    DatabaseReference messageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.poke);
+        button.setEnabled(false);
+        database = FirebaseDatabase.getInstance();
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        playerName = preferences.getString("playerName", "");
+        Bundle extras = getIntent().getExtras();
+        if (extras != null){
+            roomName = extras.getString("roomName");
+            if (roomName.equals(playerName)){
+                role = "host";
+            } else{
+                role = "quest";
+            }
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setEnabled(false);
+                message = role + ":Poked!";
+                messageRef.setValue(message);
+            }
+        });
+        //listen for incoming messages
+        messageRef = database.getReference("rooms/" + roomName + "/message");
+        message = role + ":Poked!";
+        messageRef.setValue(message);
+        addRoomEventListener();
+    }
+
+    private void addRoomEventListener(){
+        messageRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (role.equals("host")){
+                    if (dataSnapshot.getValue(String.class).contains("guest:")){
+                        button.setEnabled(true);
+                        Toast.makeText(MainActivity.this, "" +
+                                dataSnapshot.getValue(String.class).replace("guest:", ""), Toast.LENGTH_SHORT).show();
+                    }
+                } else{
+                    if (dataSnapshot.getValue(String.class).contains("host:")){
+                        button.setEnabled(true);
+                        Toast.makeText(MainActivity.this, "" +
+                                dataSnapshot.getValue(String.class).replace("guest:", ""), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // error -retry
+                messageRef.setValue(message);
+            }
+        });
     }
 }
